6616fbc140e342eaf18decd9adb33fc3bd4194c0
Nitish Shingde <ngshingde@gmail.com>
2022-09-24 00:54:47 -0600

    Add script to run on cade machine

---
+++ DecisionTree/run.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+# import ml module if not imported already
+if [ ! -d "ml" ];then
+cp -r ../ml ./
+fi
+
+# generate cars python script if it doesn't exists
+if [ ! -f "cars.py" ];then
+	jupyter nbconvert --to python cars.ipynb
+fi
+python3 cars.py
+
+#generate bank python scrip if it doesn't exists
+if [ ! -f "bank.py" ];then
+jupyter nbconvert --to python bank.ipynb
+fi
+python3 bank.py
